b2ce554797e52ebb6a55b1dce317488fda675832
FoskyM <i@fosky.top>
2024-08-30 19:27:30 +0800

    delete files

--- js/src/admin/index.ts
+++
@@ -1,5 +0,0 @@
-import app from 'flarum/admin/app';
-
-app.initializers.add('foskym/flarum-user-badges-fix', () => {
-  console.log('[foskym/flarum-user-badges-fix] Hello, admin!');
-});
--- js/src/common/index.ts
+++
@@ -1,5 +0,0 @@
-import app from 'flarum/common/app';
-
-app.initializers.add('foskym/flarum-user-badges-fix', () => {
-  console.log('[foskym/flarum-user-badges-fix] Hello, forum and admin!');
-});
